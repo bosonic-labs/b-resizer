71e8de1f9dfe6e5268b826c9b66af2f2dd08dec9
Raphaël Rougeron <goldoraf@gmail.com>
2014-04-13 09:50:17 +0200

    It now works in the four directions

--- dist/b-resizer.js
+++ dist/b-resizer.js
@@ -1,9 +1,55 @@
 (function () {
     var BResizerPrototype = Object.create(HTMLElement.prototype, {
+            left: {
+                enumerable: true,
+                get: function () {
+                    return this.hasAttribute('left');
+                }
+            },
+            right: {
+                enumerable: true,
+                get: function () {
+                    return this.hasAttribute('right');
+                }
+            },
+            vertical: {
+                enumerable: true,
+                get: function () {
+                    return this.left || this.right;
+                }
+            },
+            top: {
+                enumerable: true,
+                get: function () {
+                    return this.hasAttribute('top');
+                }
+            },
+            bottom: {
+                enumerable: true,
+                get: function () {
+                    return this.hasAttribute('bottom');
+                }
+            },
+            horizontal: {
+                enumerable: true,
+                get: function () {
+                    return this.top || this.bottom;
+                }
+            },
             createdCallback: {
                 enumerable: true,
                 value: function () {
+                    if (this.vertical) {
+                        this.setAttribute('vertical', '');
+                    }
+                    if (this.horizontal) {
+                        this.setAttribute('horizontal', '');
+                    }
+                    this.dimension = this.vertical ? 'width' : 'height';
                     this.parentNode.classList.add('b-resizer-container');
+                    if (this.left) {
+                        this.setPosition(this.parentNode, 'left', 0);
+                    }
                     this.startListener = this.start.bind(this);
                     this.addEventListener('mousedown', this.startListener, false);
                 }
@@ -22,9 +68,27 @@
             resize: {
                 enumerable: true,
                 value: function (e) {
-                    var x = e.pageX, xdiff = x - this.previousPosition.x;
-                    this.updateSize(this.parentNode, 'width', xdiff);
+                    var diff = this.getPositionDiff(e);
+                    if (this.vertical) {
+                        this.updateSize(this.parentNode, this.dimension, diff.x);
+                        if (this.left) {
+                            this.updatePosition(this.parentNode, 'left', -diff.x);
+                        }
+                    }
+                    if (this.horizontal) {
+                        this.updateSize(this.parentNode, this.dimension, this.top ? -diff.y : diff.y);
+                    }
                     this.refreshPreviousPosition(e);
+                }
+            },
+            getPositionDiff: {
+                enumerable: true,
+                value: function (e) {
+                    var x = e.pageX, y = e.pageY, xdiff = this.left ? this.previousPosition.x - x : x - this.previousPosition.x, ydiff = y - this.previousPosition.y;
+                    return {
+                        x: xdiff,
+                        y: ydiff
+                    };
                 }
             },
             refreshPreviousPosition: {
@@ -55,6 +119,25 @@
                     this.setSize(node, dimension, currentSize + diff);
                 }
             },
+            getPosition: {
+                enumerable: true,
+                value: function (node, side) {
+                    return parseInt(node.style[side].replace('px', ''));
+                }
+            },
+            setPosition: {
+                enumerable: true,
+                value: function (node, side, value) {
+                    node.style[side] = value + 'px';
+                }
+            },
+            updatePosition: {
+                enumerable: true,
+                value: function (node, side, diff) {
+                    var currentPos = this.getPosition(node, side);
+                    this.setPosition(node, side, currentPos + diff);
+                }
+            },
             stop: {
                 enumerable: true,
                 value: function (e) {
@@ -64,16 +147,4 @@
             }
         });
     window.BResizer = document.registerElement('b-resizer', { prototype: BResizerPrototype });
-    Object.defineProperty(BResizerPrototype, 'template', {
-        get: function () {
-            var fragment = document.createDocumentFragment();
-            var div = fragment.appendChild(document.createElement('div'));
-            div.innerHTML = ' <div class="b-resizer-handle"></div> ';
-            while (child = div.firstChild) {
-                fragment.insertBefore(child, div);
-            }
-            fragment.removeChild(div);
-            return { content: fragment };
-        }
-    });
 }());
